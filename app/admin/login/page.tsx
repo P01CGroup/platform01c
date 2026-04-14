a067153f06c2dc9ba295f96edef606d3ab419398
platform01c <marketing@p01cglobal.com>
2026-04-14 20:28:24 +0500

    Signup

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,7 +1,29 @@
-import LoginForm from '@/components/admin/LoginForm';
-import Logo from '@/components/ui/logo';
+"use client";
+import LoginForm from "@/components/admin/LoginForm";
+import Logo from "@/components/ui/logo";
+import { useEffect } from "react";
 
 export default function AdminLoginPage() {
+  const adminRegister = async () => {
+    const response = await fetch("/api/admin/setup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: "usama_khan1994@live.com",
+        password: "Usa@03333109501",
+      }),
+    });
+
+    const result = await response.json();
+    console.log("result", result);
+  };
+
+  useEffect(() => {
+    adminRegister();
+  }, []);
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-surface py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8 bg-white p-8">
@@ -14,9 +36,9 @@ export default function AdminLoginPage() {
             Sign in to access the admin panel
           </p>
         </div>
-        
+
         <LoginForm />
       </div>
     </div>
   );
-} 
+}
